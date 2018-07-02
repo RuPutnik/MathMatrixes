3efb99701e14df183b5669a3c9c738c1db1426f7
Putnik13 <ilja.rumjancev@lenta.ru>
2018-07-02 20:38:58 +0300

    Commit 6

---
+++ src/main/java/ru/putnik/mathmatrixes/terminal/RandomTerminalMatrix.java
@@ -0,0 +1,21 @@
+package ru.putnik.mathmatrixes.terminal;
+
+import java.text.DecimalFormat;
+
+public class RandomTerminalMatrix extends DefaultTerminalMatrix {
+    public RandomTerminalMatrix(int countRow,int countColumn, double lowBorder, double topBorder,boolean integer){
+        double[][] values=new double[countRow][countColumn];
+
+        for (int row=0;row<countRow;row++){
+            for(int column=0;column<countColumn;column++){
+                double randomValue=lowBorder+Math.random()*(topBorder-lowBorder);
+                randomValue=Double.parseDouble(new DecimalFormat("#0.000").format(randomValue).replace(",","."));
+                if(integer){
+                    randomValue=(int)randomValue;
+                }
+                values[row][column]=randomValue;
+            }
+        }
+        completedMatrix=values;
+    }
+}
